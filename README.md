7605e8524b57d6b5bf45ccd008f7497ab4fd093f
nrahman <nrahman@postmedia.com>
2016-11-19 13:35:44 -0500

    added coffee

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # gulpgoodies
 Simple customizable gulp configurations
 
-Simple clone or copy/paste gulpfile.js, customize it, and use it.
+Simply clone or copy/paste gulpfile.js, customize it, and use it.
+
+###basic
+1. Run the followign commands to install required plugins
+
+    npm install gulp-less
+    npm install gulp-sass
+
+2. Run the following command of command line
+
+    gulp runbasic
--- basic/gulpfile.js
+++ basic/gulpfile.js
@@ -1,16 +1,30 @@
 // include plugins
 var gulp = require('gulp'),
 	gless = require("gulp-less"),
-	gsass = require("gulp-sass");
+	gsass = require("gulp-sass"),
+	gcoffee = require('gulp-coffee');
  
 // compile less to css
 gulp.task('lesstocss', function () {
-	gulp.src('less/less.less').pipe(gless()).pipe(gulp.dest('css'));
+	gulp.src('less/*.less').pipe(gless()).pipe(gulp.dest('css'));
 });
 
 // compile sass to css
 gulp.task('sasstocss', function () {
-	gulp.src('sass/sass.scss').pipe(gless()).pipe(gulp.dest('css'));
+	gulp.src('sass/*.scss').pipe(gless()).pipe(gulp.dest('css'));
 });
 
-gulp.task('runbasic', ['lesstocss', 'sasstocss']);
+gulp.task('drinkcoffee', function () {
+	gulp.src('coffee/*.coffee').pipe(gcoffee()).pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function() {
+    //gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sasstocss']);
+    gulp.watch('less/*.less', ['lesstocss']);
+    gulp.watch('coffee/*.coffee', ['drinkcoffee']);
+});
+
+// sequence tasks
+gulp.task('default', ['lesstocss', 'sasstocss', 'drinkcoffee', 'watch']);
+
